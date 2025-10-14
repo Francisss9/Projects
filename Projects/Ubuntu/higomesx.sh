78760d96cfcbc4bbde9062f39786ea74e706995b
Francisgomesx <80554120+Francisss9@users.noreply.github.com>
2025-10-14 20:44:38 +0100

    Created higomesx.sh

---
+++ Projects/Ubuntu/higomesx.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+name=$1
+
+echo "Hi $name"
+
+sleep 1
+
+
+echo "Let's start coding!"
+
+read answer
+
+if [[ $answer == "Yes" ]]; then
+
+ echo "Let's go!"
+ sleep 1
+
+else
+
+ echo "Maybe next time!"
+fi
